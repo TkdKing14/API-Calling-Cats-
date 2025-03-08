7900f322a0ab7d13fcd34e92b2dcfd1a43898d44
Carson Payne <carson.j.payne14@gmail.com>
2025-03-08 14:36:17 -0600

    Initial Commit

---
+++ API Calling (Cats)/API_Calling__Cats_App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling__Cats_App.swift
+//  API Calling (Cats)
+//
+//  Created by Carson Payne on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling__Cats_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
